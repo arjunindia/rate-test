d7dc988975ede2d5692a28de4fa372de26a15114
Arjun S <arjun8107@gmail.com>
2025-02-02 17:26:42 +0530

    sled

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
-use axum::{response, routing::get, Router};
-use std::{thread::sleep, time};
+use axum::{
+    extract::{Path, State},
+    response,
+    routing::get,
+    Router,
+};
+use sled::Db;
+use std::sync::Arc;
 use tokio::net::TcpListener;
 static BODY: &'static str = include_str!("../hello.html");
 static _404: &'static str = include_str!("../404.html");
+struct AppState {
+    db: Db,
+}
+#[axum::debug_handler]
+async fn handle_db_read(
+    Path(id): Path<String>,
+    State(appstate): State<Arc<AppState>>,
+) -> response::Html<String> {
+    let key = id.parse::<u64>().unwrap().to_be_bytes();
+    let value = appstate.db.get(key).unwrap().unwrap();
+    response::Html(format!("Value: {}", String::from_utf8_lossy(&value)))
+}
 
 #[tokio::main]
 async fn main() {
+    let appstate = Arc::new(AppState {
+        db: sled::open("my_db").unwrap(),
+    });
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { response::Html(BODY) }))
         .route(
-            "/sleep",
-            get(|| async {
-                sleep(time::Duration::from_secs(5));
-                response::Html(BODY)
+            "/db",
+            get(|appstate: State<Arc<AppState>>| async move {
+                let db = &appstate.clone().db;
+                let id = db.generate_id().unwrap();
+                db.insert(&id.to_be_bytes(), &id.to_be_bytes()).unwrap();
+                response::Html(format!("Inserted key: {}", id))
             }),
         )
-        .route("/404", get(|| async { response::Html(_404) }));
+        .route("/db/{id}", get(handle_db_read))
+        .route("/404", get(|| async { response::Html(_404) }))
+        .with_state(appstate);
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
