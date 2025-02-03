6609b7a3084c876054c2b6640f60b1a50acaa0c2
Arjun S <arjun8107@gmail.com>
2025-02-03 21:01:25 +0530

    surrealkv

--- src/main.rs
+++ src/main.rs
@@ -7,51 +7,79 @@ use axum::{
 use sled::Db;
 use std::{
     net::{IpAddr, Ipv4Addr},
-    sync::Arc,
+    sync::{atomic::AtomicU32, Arc},
 };
+use surrealkv::{Options, Store};
 use tokio::net::TcpListener;
 static BODY: &'static str = include_str!("../hello.html");
 static _404: &'static str = include_str!("../404.html");
+pub type SharedState = Arc<RwLock<AppState>>;
+
 struct AppState {
     db: Db,
+    store: Store,
+    id: u32,
 }
 
 async fn handle_db_read(
     Path(id): Path<String>,
     State(appstate): State<Arc<AppState>>,
 ) -> response::Html<String> {
-    let key = id.parse::<u64>().unwrap();
-    let value = appstate
-        .db
-        .get(key.to_be_bytes())
-        .unwrap()
-        .unwrap_or_default();
-    let value = value
-        .to_vec()
-        .into_iter()
-        .map(|b| b as char)
-        .collect::<String>();
-
-    // let key = id.parse::<u64>().unwrap().to_be_bytes();
-    // let value = appstate.db.get(key).unwrap().unwrap();
+    let key = id.parse::<i32>().unwrap().to_be_bytes();
+    let mut value: String;
+    appstate
+        .store
+        .view(|txn| {
+            let id = txn
+                .get(&key)
+                .unwrap()
+                .map(|v| String::from_utf8(v.to_vec()).unwrap());
+            value = id.unwrap();
+            Ok(())
+        })
+        .unwrap();
     response::Html(format!("Value: {}", value))
 }
 
 #[tokio::main]
 async fn main() {
+    // Create a new store
+    let mut opts = Options::new();
+    opts.dir = "./data".into();
+    let store = Store::new(opts).expect("failed to create store");
+    let mut id: u32 = 0;
+    store
+        .view(|txn| {
+            let counter = txn.get(b"counter").unwrap();
+            if let Some(counter) = counter {
+                id = u32::from_be_bytes(counter.to_vec().try_into().unwrap());
+            }
+            Ok(())
+        })
+        .unwrap();
+
+    let state = SharedState::default();
     let appstate = AppState {
         db: sled::open("my_db").unwrap(),
+        store,
+        id,
     };
+
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { response::Html(BODY) }))
         .route(
             "/db",
-            get(|appstate: State<Arc<AppState>>| async move {
-                let db = &appstate.db;
-                let id = db.generate_id().unwrap();
-                db.insert(id.to_be_bytes(), b"Hello, World!").unwrap();
-                response::Html(format!("Inserted: {}", id))
+            get(|mut appstate: State<Arc<AppState>>| async move {
+                let writable = state.write().unwrap();
+                let store = &appstate.store;
+                let mut txn = store.begin_with_mode(surrealkv::Mode::WriteOnly).unwrap();
+                appstate.id += 1;
+                let key = appstate.id.to_be_bytes();
+                txn.set(&key, b"Hello, World!").unwrap();
+                txn.set(b"counter", &key).unwrap();
+                txn.commit().await.unwrap();
+                response::Html(format!("Inserted: {}", appstate.id))
             }),
         )
         .route("/db/{id}", get(handle_db_read))
