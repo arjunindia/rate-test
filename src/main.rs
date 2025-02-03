01c6d5f5fd2c4423be071f1d229b8e053c8adce4
Arjun S <arjun8107@gmail.com>
2025-02-03 16:46:44 +0530

    removing redis cuz issues

--- src/main.rs
+++ src/main.rs
@@ -4,87 +4,43 @@ use axum::{
     routing::get,
     Router,
 };
-use fred::{clients::Pool, prelude::*};
-
 use sled::Db;
 use std::{
-    env,
     net::{IpAddr, Ipv4Addr},
     sync::Arc,
-    time::Duration,
 };
 use tokio::net::TcpListener;
 static BODY: &'static str = include_str!("../hello.html");
 static _404: &'static str = include_str!("../404.html");
 struct AppState {
     db: Db,
-    pool: Option<Pool>,
 }
 
 async fn handle_db_read(
     Path(id): Path<String>,
     State(appstate): State<Arc<AppState>>,
 ) -> response::Html<String> {
-    let val = if let Some(pool) = &appstate.pool {
-        pool.get::<String, &str>(&id).await
-    } else {
-        Err(fred::error::Error::new(
-            fred::error::ErrorKind::NotFound,
-            "Not Found".to_string(),
-        ))
-    };
-    let val = match val {
-        Ok(v) => v,
-        Err(_) => {
-            let key = id.parse::<u64>().unwrap();
-            let value = appstate
-                .db
-                .get(key.to_be_bytes())
-                .unwrap()
-                .unwrap_or_default();
-            let value = value
-                .to_vec()
-                .into_iter()
-                .map(|b| b as char)
-                .collect::<String>();
-            if let Some(pool) = &appstate.pool {
-                pool.set::<(), u64, &str>(key, &value, None, None, false)
-                    .await
-                    .unwrap();
-            }
-            value
-        }
-    };
+    let key = id.parse::<u64>().unwrap();
+    let value = appstate
+        .db
+        .get(key.to_be_bytes())
+        .unwrap()
+        .unwrap_or_default();
+    let value = value
+        .to_vec()
+        .into_iter()
+        .map(|b| b as char)
+        .collect::<String>();
+
     // let key = id.parse::<u64>().unwrap().to_be_bytes();
     // let value = appstate.db.get(key).unwrap().unwrap();
-    response::Html(format!("Value: {}", val))
+    response::Html(format!("Value: {}", value))
 }
 
 #[tokio::main]
 async fn main() {
-    let pool_size = env::var("REDIS_POOL_SIZE")
-        .ok()
-        .and_then(|v| v.parse::<usize>().ok())
-        .unwrap_or(8);
-    let redis_url = "redis://127.0.0.1:6379";
-    let config = Config::from_url(&redis_url).unwrap();
-    let pool = Builder::from_config(config)
-        .with_connection_config(|config| {
-            config.connection_timeout = Duration::from_secs(10);
-        })
-        // use exponential backoff, starting at 100 ms and doubling on each failed attempt up to 30 sec
-        .set_policy(ReconnectPolicy::new_exponential(0, 100, 30_000, 2))
-        .build_pool(pool_size);
-    let pool = match pool {
-        Ok(pool) => match pool.init().await {
-            Ok(_) => Some(pool),
-            Err(_) => None,
-        },
-        Err(_) => None,
-    };
     let appstate = AppState {
         db: sled::open("my_db").unwrap(),
-        pool,
     };
     // build our application with a single route
     let app = Router::new()
